745df7224a2f2c792b79e67296a737554265a21c
Daniil <trumangry@gmail.com>
2019-12-14 16:43:02 +0300

    project making + component adding + component ajaxing

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,36 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!-- <img alt="Vue logo" src="./assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+    
+    <UserCard v-if="user" :user="user"></UserCard>
+    {{user}}
+  
+    
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import UserCard from'./components/UserCard.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+
+    UserCard
+  },
+  data(){
+    return {
+      user: null
+    }
+  },
+  mounted() {
+    this.axios.get('https://randomuser.me/api').then((response)=>{
+      console.log('response', response.data)
+      this.user = response.data.results[0]
+    }).catch(err=>{
+      console.log('errr', err)
+    })
   }
 }
 </script>
